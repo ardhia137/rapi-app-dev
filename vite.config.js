5dbf4f889f1eb78a39e924c647fdee363ec16853
ardhi <ardhia137@gmail.com>
2025-12-31 09:58:10 +0700

    docker

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,21 @@ import {
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: true,
-        port: 5173,
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // Gunakan relative path untuk assets
+    base: '/',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
